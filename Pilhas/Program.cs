8f784896daaf4c0f2bfeec391cf071660d8352b5
Erbson Lavras <erbson.org@gmail.com>
2020-11-17 14:13:46 -0300

    Adicionar arquivos de projeto.

---
+++ Pilhas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pilhas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<String> Pilha = new Stack<string>();
+            //formando a Pilha com o metodo Push
+
+            Pilha.Push("NUMERO01");
+            Pilha.Push("NUMERO002");
+            Pilha.Push("NUMERO03");
+            Pilha.Push("NUMERO04");
+            Pilha.Push("NUMERO05");
+
+            //Removendo o ultimo da pilha
+            Pilha.Pop();
+
+            // Mostrando os intens da Pilha
+            foreach(String i  in Pilha)
+            {
+                Console.WriteLine(i);
+
+            }
+           
+
+            Console.ReadKey();
+        }
+    }
+}
